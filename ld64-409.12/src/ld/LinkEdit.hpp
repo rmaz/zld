584ab5fefb2ad2b52e80ccd7d747e3575e63ac44
Michael Eisel <michael.eisel@gmail.com>
2020-02-10 12:42:44 -0500

    fix

--- ld64-409.12/src/ld/LinkEdit.hpp
+++ ld64-409.12/src/ld/LinkEdit.hpp
@@ -1320,11 +1320,8 @@ void ExportInfoAtom<A>::encode() const
 	}
 
 	// sort vector by -exported_symbols_order, and any others by address
-	//if _options.ported
-	if (_options.hasExportedSymbolOrder()) {
-    	std::sort(entries.begin(), entries.end(), TrieEntriesSorter(_options));
-	}
-	
+	std::sort(entries.begin(), entries.end(), TrieEntriesSorter(_options));
+
 	// create trie
 	mach_o::trie::makeTrie(entries, this->_encodedData.bytes());
 
